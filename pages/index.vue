90ea5f3df933387e0a66cb9a2e58b34094cd6b5e
unknown <anisouza976@gmail.com>
2022-06-27 16:44:51 -0300

    Primeiro commit do projeto Studif

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+<body>
+  <div class="login">
+    <barraNav></barraNav>
+    <center> <h2> Log in </h2></center>
+    <b-container>
+      <label> <h4> E-mail </h4></label>
+      <b-form-input placeholder="mariadasilva.r@gmail.com"></b-form-input>
+      <label> <h4> Senha </h4> </label>
+      <b-form-input placeholder="Ex: ******"></b-form-input>
+      <b-button class="botão"> login </b-button>
+    </b-container>
+  </div>
+</body>
+</template>
+
+<script>
+import barraNav from '~/components/barraNav.vue'
+export default {
+  components: { barraNav },
+  name: 'IndexPage',
+}
+</script>
+<style>
+
+body{
+  /* background: rgb(255,255,255); */
+  background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(204,255,164,1) 35%, rgba(159,214,117,1) 100%);
+  background-repeat: no-repeat;
+  height: 120vh;
+}
+
+.botão{
+  width: 15%;
+  right: 20%;
+  background-color: #589b23;
+  border-color: black;
+  border-radius: 40px;
+  margin: 0 auto;
+  color: rgb(0, 0, 0);
+  display: block;
+  padding: 10px;
+  margin-top: 20px;
+}
+</style>  
---
+++ pages/tela-dica.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <barraNav></barraNav>
+    <b-container class="container">
+      <img src="lampada.webp" width="320" height="205" />
+      <center><p>Dica do dia</p></center>
+
+      <b-container class="DicadoDia">
+        <p>
+          Leia, mas escreva tambem. O estudo feito só por meio da leitura não é
+          tão eficiente. O estudante pode se pegar facilmente distraido no
+          decorrer das páginas. Por isso, escreva! Faça esquemas, transcreva
+          partes importantes e organize resumos. Quem escreve consegue assimilar
+          melhor o conteúdo e tê-lo por mais tempo na mente.
+        </p>
+      </b-container>
+
+      <b-container class="BancodeDicas">
+        <center><p>Banco de Dicas</p></center>
+      </b-container>
+      <b-container class="AddDicas">
+        <center><p>+ Adicionar Dicas</p></center>
+      </b-container>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import barraNav from '~/components/barraNav.vue'
+export default {
+  components: { barraNav },
+  name: 'IndexPage',
+}
+</script>
+<style>
+html,
+body {
+  height: 100%;
+  width: 100%;
+  background-color: rgb(255, 255, 255);
+}
+.container img {
+  max-width: 200px;
+  display: block;
+  margin: 0 auto;
+}
+.DicadoDia,
+.BancodeDicas,
+.AddDicas {
+  background-color: #C9E9AF;
+}
+</style>
